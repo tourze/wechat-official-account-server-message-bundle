22fdac6accddc00a04e56f1370ac100e5774ecc4
github-ci <you@example.com>
2025-05-18 10:51:32 +0000

    Revert "refactor(test): 添加模拟官方账号类并增强测试"

--- src/MessageHandler/ServerCallbackHandler.php
+++ src/MessageHandler/ServerCallbackHandler.php
@@ -7,7 +7,6 @@ use Symfony\Component\Lock\LockFactory;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 use Tourze\DoctrineUpsertBundle\Service\UpsertManager;
-use Tourze\WechatOfficialAccountContracts\OfficialAccountInterface;
 use Tourze\WechatOfficialAccountContracts\UserLoaderInterface;
 use WechatOfficialAccountBundle\Repository\AccountRepository;
 use WechatOfficialAccountServerMessageBundle\Entity\ServerMessage;
@@ -39,28 +38,12 @@ class ServerCallbackHandler
         }
 
         $account = $this->accountRepository->find($asyncMessage->getAccountId());
-        if ($account === null) {
-            $this->logger->error('找不到对应的公众号账号', [
-                'accountId' => $asyncMessage->getAccountId(),
-            ]);
-            $lock->release();
-            return null;
-        }
 
         try {
             // 不管事件内怎么处理，我们先自己保证存一份消息
             $localMsg = ServerMessage::createFromMessage($message);
             $localMsg->setAccount($account);
             $this->upsertManager->upsert($localMsg);
-
-            // 确保account实现了OfficialAccountInterface接口
-            if (!$account instanceof OfficialAccountInterface) {
-                throw new \TypeError(sprintf(
-                    'Account must implement %s, %s given.',
-                    OfficialAccountInterface::class,
-                    get_class($account)
-                ));
-            }
 
             // 因为在这里我们也能拿到OpenID了，所以同时也要存库一次
             $localUser = $this->userLoader->syncUserByOpenId($account, $message['FromUserName']);
