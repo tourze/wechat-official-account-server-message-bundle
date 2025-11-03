f14180878479aa3365109b88bfca53943ebbf1f4
github-ci <you@example.com>
2025-11-03 14:27:39 +0800

    refactor(user-agreement-bundle): 解耦 BizUserBundle，使用 user-service-contracts 接口

--- src/WechatOfficialAccountServerMessageBundle.php
+++ src/WechatOfficialAccountServerMessageBundle.php
@@ -8,6 +8,7 @@ use Tourze\BundleDependency\BundleDependencyInterface;
 use Tourze\EasyAdmin\Attribute\Permission\AsPermission;
 use Tourze\WechatOfficialAccountOAuth2Bundle\WechatOfficialAccountOAuth2Bundle;
 use WechatOfficialAccountBundle\WechatOfficialAccountBundle;
+use Tourze\EasyAdminMenuBundle\EasyAdminMenuBundle;
 
 #[AsPermission(title: '服务端消息')]
 class WechatOfficialAccountServerMessageBundle extends Bundle implements BundleDependencyInterface
@@ -18,6 +19,7 @@ class WechatOfficialAccountServerMessageBundle extends Bundle implements BundleD
             WechatOfficialAccountBundle::class => ['all' => true],
             WechatOfficialAccountOAuth2Bundle::class => ['all' => true],
             DoctrineBundle::class => ['all' => true],
+            EasyAdminMenuBundle::class => ['all' => true],
         ];
     }
 }
